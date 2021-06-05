8990dfbd6bd804753242ce61443d19cdbacaf2df
kabiro <kabiryves90@gmail.com>
2021-06-05 20:52:58 +0200

    modify autochange.js for vehicule js

--- public/js/autoCharge.js
+++ public/js/autoCharge.js
@@ -1,10 +1,11 @@
+$(document).ready(function () {
+  $('#marque_id').on('change', function () {
+console.log("tweensContainer");
+    let marque_id = $(this).val();
+    $.get(route + '/vehicules/chargeMarque', { marque_id: marque_id }, function (data) {
+      $('#modele_id').html(data);
+    });
 
-    $(document).ready(function(){
-    $('#marque_id').on('change',function(){
-
-var marque_id=$(this).val();
-$.get(route + '/vehicules/chargeMarque', {marque_id:marque_id},function(data){
-  $('#modele_id').html(data);
+  });
 });
 
-});
