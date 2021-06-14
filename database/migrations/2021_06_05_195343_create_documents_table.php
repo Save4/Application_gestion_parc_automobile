64d94c5060eb32bbbdaaad52dbcbbb088a01e9d9
kabiro <kabiryves90@gmail.com>
2021-06-14 15:00:13 +0200

    add bd document

---
+++ database/migrations/2021_06_05_195343_create_documents_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDocumentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('documents', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('vehicule_id')->nullable();
+            $table->unsignedBigInteger('fournisseur_id')->nullable();
+            $table->string('fileName');
+            $table->string('file');
+            $table->date('debut_validite')->nullable();
+            $table->date('fin_validite')->nullable();
+            $table->integer('prix')->nullable();
+            $table->timestamps();
+            $table->foreign('vehicule_id')
+                ->references('id')
+                ->on('vehicules')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->foreign('fournisseur_id')
+                ->references('id')
+                ->on('fournisseurs')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('documents');
+    }
+}
