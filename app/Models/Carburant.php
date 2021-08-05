3f6d599fe64a52be58b12fed6c185d0124b50676
kabiro <kabiryves90@gmail.com>
2021-08-05 19:53:06 +0200

    aout des donnees dans le model carburant

--- app/Models/Carburant.php
+++ app/Models/Carburant.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Carburant extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'vehicule_id','type_carburant',
+        'quantite','prix_unitaire',
+        'prix_total','distance',
+        'distance_litre'
+    ];
 }
