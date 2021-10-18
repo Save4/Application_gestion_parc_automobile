fcf2bf33f88e3ea6af260e157c17c313160e6c65
kabiro <kabiryves90@gmail.com>
2021-10-18 12:29:25 +0200

    ajout de la connection de la table fournisseurs avec users

--- database/migrations/2021_05_22_130927_create_fournisseurs_table.php
+++ database/migrations/2021_05_22_130927_create_fournisseurs_table.php
@@ -19,7 +19,13 @@ class CreateFournisseursTable extends Migration
             $table->string('adresse_fournisseur');
             $table->string('phone_fournisseur');
             $table->string('email_fournisseur');
+            $table->unsignedBigInteger('user_id');
             $table->timestamps();
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
         });
     }
 
