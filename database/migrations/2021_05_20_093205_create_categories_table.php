ac20f9dfd84258c6a371d83e769f8f81660fe7e6
kabiro <kabiryves90@gmail.com>
2021-10-18 12:27:50 +0200

    ajout de la connection de la table categories avec users

--- database/migrations/2021_05_20_093205_create_categories_table.php
+++ database/migrations/2021_05_20_093205_create_categories_table.php
@@ -16,7 +16,13 @@ class CreateCategoriesTable extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('nom_category');
+            $table->unsignedBigInteger('user_id');
             $table->timestamps();
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
         });
     }
 
