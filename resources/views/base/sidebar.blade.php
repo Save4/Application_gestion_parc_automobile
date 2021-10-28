165bea34b1abdc9084a23a117e86f2a03bb19b40
kabiro <kabiryves90@gmail.com>
2021-10-29 01:05:06 +0200

    modification du logo dans sidebar

--- resources/views/base/sidebar.blade.php
+++ resources/views/base/sidebar.blade.php
@@ -1,8 +1,8 @@
 <aside class="main-sidebar sidebar-light-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="#" class="brand-link">
-        <img src="{{ asset('dist/img/AdminLTELogo.png') }}" alt="Automobile"
-            class="brand-image img-circle elevation-3" style="opacity: .8">
+    <a href="{{ url('/home') }}" class="brand-link">
+        <img src="{{ asset('dist/img/logo.png') }}" alt="Automobile" class="brand-image img-circle elevation-3"
+            style="opacity: .8">
         <span class="brand-text font-weight-light">Parc automobile</span>
     </a>
 
